eafc0fed95269dbd93507e09e00b8174de9215d5
Mariana Dominguez <maridoh@gmail.com>
2020-03-26 17:49:51 -0700

    dashboard working and updating live

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Loading extends Component { 
+
+  render () {
+    return <section className="loading">Loading</section>;
+  }
+}
+
+export default Loading;
